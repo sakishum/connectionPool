cb4102064c2bf029701c8ece793cd4a8ea65947e
sakishum <sakishum11118@gmail.com>
2013-03-06 17:47:51 +0800

    Added some comments.

--- connect_pool.cpp
+++ connect_pool.cpp
@@ -13,13 +13,23 @@
 using namespace std;
 using namespace sql;
 
+/// 全局变量 
 const string g_sdefaultHost = "localhost";
 const string g_sdefaultUser = "root";
 const string g_sdefaultPass = "qwer1234";
 const int g_imaxSize = 50;
 
+/// 单例声明
 auto_ptr<CConnctionPool> CConnctionPool::auto_ptr_instance;
 
+/**
+ * @Synopsis  CConnctionPool 
+ *	连接池构造函数
+ * @Param url
+ * @Param userName
+ * @Param password
+ * @Param maxSize
+ */
 CConnctionPool::CConnctionPool(string url, string userName, string password, int maxSize)
 :m_imaxSize(maxSize), m_icurSize(0), m_suserName(userName), m_spassword(password), m_surl(url)
 {
@@ -35,11 +45,20 @@ CConnctionPool::CConnctionPool(string url, string userName, string password, int
 	this->InitConnection(m_imaxSize/2);
 }
 
+/**
+ * @Synopsis  ~CConnctionPool 
+ *	连接池析构函数
+ */
 CConnctionPool::~CConnctionPool(void)
 {
 	this->TerminateConnectionPool();
 }
 
+/**
+ * @Synopsis  GetInstance 
+ *	获取连接池对象，单例模式
+ * @Returns   连接池对象
+ */
 CConnctionPool& CConnctionPool::GetInstance(void)
 {
 	if (auto_ptr_instance.get() == 0) {
@@ -48,6 +67,11 @@ CConnctionPool& CConnctionPool::GetInstance(void)
 	return *(auto_ptr_instance.get());
 }
 
+/**
+ * @Synopsis  InitConnection 
+ *	初始化连接池，创建最大连接数的一半连接量
+ * @Param iInitialSize
+ */
 void CConnctionPool::InitConnection(int iInitialSize)
 {
 	Connection *pConn;
@@ -64,6 +88,11 @@ void CConnctionPool::InitConnection(int iInitialSize)
 	pthread_mutex_unlock(&lock);
 }
 
+/**
+ * @Synopsis  CreateConnection 
+ *	创建连接，返回一个 Connection
+ * @Returns   sql::connection*
+ */
 Connection* CConnctionPool::CreateConnection(void)
 {
 	Connection *pConn;
@@ -81,23 +110,32 @@ Connection* CConnctionPool::CreateConnection(void)
 	}*/
 }
 
+/**
+ * @Synopsis  GetConnection 
+ *	在连接池中获取一个连接
+ * @Returns   sql::connection*
+ */
 Connection* CConnctionPool::GetConnection(void)
 {
 	Connection *pConn;
 	pthread_mutex_lock(&lock);
+	/// 连接池容器中还有连接
 	if (m_connlist.size() > 0) {
-		pConn = m_connlist.front();
-		m_connlist.pop_front();
+		pConn = m_connlist.front();		/// 得到第一个连接
+		m_connlist.pop_front();			/// 移除第一个连接
+		/// 如果连接池已经关闭，删除后重新建立一个
 		if (pConn->isClosed()) {
 			delete pConn;
 			pConn = this->CreateConnection();
 		}
+		/// 如果连接为空，则创建连接出错
 		if (NULL == pConn) {
 			--m_icurSize;
 		}
 		pthread_mutex_unlock(&lock);
 		return pConn;
 	} else {
+		/// 还可以创建新的连接
 		if (m_icurSize < m_imaxSize) {
 			pConn = this->CreateConnection();
 			if (pConn) {
@@ -109,12 +147,18 @@ Connection* CConnctionPool::GetConnection(void)
 				return NULL;
 			}
 		} else {
+			/// 建立的连接数已达到 m_imaxSize 上限
 			pthread_mutex_unlock(&lock); 
 			return NULL;
 		}
 	}
 }
 
+/**
+ * @Synopsis  ReleaseConnection 
+ *	回收数据库连接
+ * @Param pConn
+ */
 void CConnctionPool::ReleaseConnection(Connection* pConn)
 {
 	if (pConn) {
@@ -124,19 +168,28 @@ void CConnctionPool::ReleaseConnection(Connection* pConn)
 	}
 }
 
+/**
+ * @Synopsis  TerminateConnectionPool 
+ *	销毁连接池，首先要先销毁连接池中的连接
+ */
 void CConnctionPool::TerminateConnectionPool(void)
 {
 	list<Connection*>::iterator iconn;
 	pthread_mutex_lock(&lock);
 	for (iconn=m_connlist.begin(); iconn != m_connlist.end(); ++iconn)
 	{
-		this->TerminateConnection(*iconn);
+		this->TerminateConnection(*iconn);	/// 销毁连接池中的连接
 	}
 	m_icurSize = 0;
-	m_connlist.clear();
+	m_connlist.clear();		/// 清空连接池中的连接
 	pthread_mutex_unlock(&lock);
 }
 
+/**
+ * @Synopsis  TerminateConnection 
+ *	销毁一个连接
+ * @Param pConn
+ */
 void CConnctionPool::TerminateConnection(Connection *pConn)
 {
 	if (pConn) {
@@ -152,6 +205,11 @@ void CConnctionPool::TerminateConnection(Connection *pConn)
 	delete pConn;
 }
 
+/**
+ * @Synopsis  GetCurrentSize 
+ *	获取当前线程池中的有效的连接数(可选)
+ * @Returns   
+ */
 int CConnctionPool::GetCurrentSize(void)
 {
 	return m_icurSize;
--- connect_pool.h
+++ connect_pool.h
@@ -26,32 +26,32 @@ using namespace sql;
 
 class CConnctionPool {
 private:
-	int m_icurSize;	
-	int m_imaxSize;
-	string m_suserName;
-	string m_spassword;
-	string m_surl;
+	int m_icurSize;		/// 当前已建立的数据库连接	
+	int m_imaxSize;		/// 连接池中定义的最大数据库连接数
+	string m_suserName;	/// 数据库用户名
+	string m_spassword;	/// 数据库用户密码
+	string m_surl;		/// 数据库连接 url 
 
-	list<Connection*>m_connlist;
-	pthread_mutex_t lock;
-	Driver *m_pdriver;
+	list<Connection*>m_connlist;	/// 连接池的容器队列
+	pthread_mutex_t lock;			/// 线程锁
+	Driver *m_pdriver;				/// 连接数据库驱动
 
-	friend class auto_ptr<CConnctionPool>;
-	static auto_ptr<CConnctionPool> auto_ptr_instance;
+	friend class auto_ptr<CConnctionPool>;				/// 使用智能指针
+	static auto_ptr<CConnctionPool> auto_ptr_instance;	/// 惟一实例
 
 private:
-	CConnctionPool(string url, string userName, string password, int maxSize);
-	~CConnctionPool(void);
-	Connection* CreateConnection(void);
-	void InitConnection(int iInitialSize);
-	void TerminateConnection(Connection* pConn);
-	void TerminateConnectionPool(void);
+	CConnctionPool(string url, string userName, string password, int maxSize);	/// 构造函数(注意:构造方法私有)
+	~CConnctionPool(void);							/// 析构函数
+	Connection* CreateConnection(void);				/// 创建一个连接
+	void InitConnection(int iInitialSize);			/// 初始化一个连接
+	void TerminateConnection(Connection* pConn);	/// 终止一个数据库连接对象
+	void TerminateConnectionPool(void);				/// 终止数据库连接池
 
 public:
-	Connection* GetConnection(void);
-	void ReleaseConnection(Connection *pConn);
-	static CConnctionPool& GetInstance(void);
-	int GetCurrentSize(void);
+	Connection* GetConnection(void);				///	获得数据库连接
+	void ReleaseConnection(Connection *pConn);		/// 将数据库连接放回到连接池的容器中
+	static CConnctionPool& GetInstance(void);		/// 获取数据库连接池实例(工厂方法)
+	int GetCurrentSize(void);	/// 用于观测
 };
 
 #endif	/*_CONNECTION_POOL_H_*/
